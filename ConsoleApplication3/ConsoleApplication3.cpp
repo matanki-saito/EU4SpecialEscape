3c9a1cc633ff71c8bd52744b25320bf701fa19fa
matanki-saito <matanki.saito@gmal.com>
2018-06-09 12:35:09 +0900

    FR

--- ConsoleApplication3/ConsoleApplication3.cpp
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -1,13 +1,16 @@
-// ConsoleApplication3.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
-//
-
 #include "stdafx.h"
 
-errno_t sub(const wchar_t *source, wchar_t *baseFileName);
+errno_t subUTF8(const wchar_t *source, wchar_t *baseFileName);
+errno_t subUTF8B(const wchar_t *source, wchar_t *baseFileName);
 
 errno_t loadTextFromBinary(const wchar_t *source, char **to) {
 
 	errno_t success = 0;
+	errno_t readFileError = 0;
+	FILE *fp;
+	int error = 0;
+	struct _stat statData;
+	int readSize = 0;
 
 	if (source == NULL) {
 		success = 1;
@@ -15,39 +18,35 @@ errno_t loadTextFromBinary(const wchar_t *source, char **to) {
 	}
 
 	/* */
-	struct _stat statData;
-	int error = _wstat(source, &statData);
-
+	error = _wstat(source, &statData);
 	if (error != NULL) {
 		success = 2;
 		goto F;
 	}
 
 	/* */
-	FILE *fp;
-	errno_t readFileError = _wfopen_s(&fp, source, L"rb");
-
+	
+	readFileError = _wfopen_s(&fp, source, L"rb");
 	if (readFileError != NULL) {
 		success = 3;
 		goto F;
 	}
 
 	/* */
-	*to = (char*) calloc(statData.st_size + 1, sizeof(char));
-
+	*to = (char*)calloc(statData.st_size + 1, sizeof(char));
 	if (*to == NULL) {
 		success = 4;
 		goto A;
 	}
 
 	/* */
-	int readSize = fread_s(*to, statData.st_size, sizeof(char), statData.st_size, fp);
+	readSize = fread_s(*to, statData.st_size, sizeof(char), statData.st_size, fp);
 
 	if (readSize != statData.st_size) {
 		success = 5;
 		goto A;
 	}
-	
+
 A:
 	fclose(fp);
 F:
@@ -57,6 +56,8 @@ F:
 errno_t removeBOM(const char *from, char **to) {
 
 	errno_t success = 0;
+	errno_t err = 0;
+	unsigned int size = 0;
 
 	/* */
 	if (from == NULL) {
@@ -71,13 +72,13 @@ errno_t removeBOM(const char *from, char **to) {
 	}
 
 	/* */
-	if (! ((from[0] == ((char)0xEF)) && (from[1] == (char)0xBB) && (from[2] == (char)0xBF)) ){
+	if (!((from[0] == ((char)0xEF)) && (from[1] == (char)0xBB) && (from[2] == (char)0xBF))) {
 		success = 3;
 		goto A;
 	}
 
 	/* */
-	unsigned int size = strlen(from);
+	size = strlen(from);
 
 	/* */
 	*to = (char*)calloc(size - 2, sizeof(char));
@@ -88,7 +89,7 @@ errno_t removeBOM(const char *from, char **to) {
 	}
 
 	/* */
-	errno_t err = memcpy_s(*to,size-2, from + 3, size - 3);
+	err = memcpy_s(*to, size - 2, from + 3, size - 3);
 
 	if (err) {
 		success = 5;
@@ -107,7 +108,9 @@ A:
 errno_t convertTextToWideText(const char* from, wchar_t **to) {
 
 	errno_t success = 0;
-	
+	unsigned int err = 0;
+	unsigned int wideTextSize = 0;
+
 	/* */
 	if (from == NULL) {
 		success = 1;
@@ -115,7 +118,7 @@ errno_t convertTextToWideText(const char* from, wchar_t **to) {
 	}
 
 	/* */
-	unsigned int wideTextSize = MultiByteToWideChar(
+	wideTextSize = MultiByteToWideChar(
 		CP_UTF8,
 		NULL,
 		from,
@@ -137,7 +140,7 @@ errno_t convertTextToWideText(const char* from, wchar_t **to) {
 	}
 
 	/* */
-	unsigned int err = MultiByteToWideChar(
+	err = MultiByteToWideChar(
 		CP_UTF8,
 		NULL,
 		from,
@@ -156,6 +159,41 @@ B:
 	free(*to);
 A:
 	return success;
+}
+
+inline wchar_t UCS2ToCP1252(int cp) {
+	wchar_t result = cp;
+	switch (cp) {
+	case 0x20AC: result = 0x80; break;
+	case 0x201A: result = 0x82; break;
+	case 0x0192: result = 0x83; break;
+	case 0x201E: result = 0x84; break;
+	case 0x2026: result = 0x85; break;
+	case 0x2020: result = 0x86; break;
+	case 0x2021: result = 0x87; break;
+	case 0x02C6: result = 0x88; break;
+	case 0x2030: result = 0x89; break;
+	case 0x0160: result = 0x8A; break;
+	case 0x2039: result = 0x8B; break;
+	case 0x0152: result = 0x8C; break;
+	case 0x017D: result = 0x8E; break;
+	case 0x2018: result = 0x91; break;
+	case 0x2019: result = 0x92; break;
+	case 0x201C: result = 0x93; break;
+	case 0x201D: result = 0x94; break;
+	case 0x2022: result = 0x95; break;
+	case 0x2013: result = 0x96; break;
+	case 0x2014: result = 0x97; break;
+	case 0x02DC: result = 0x98; break;
+	case 0x2122: result = 0x99; break;
+	case 0x0161: result = 0x9A; break;
+	case 0x203A: result = 0x9B; break;
+	case 0x0153: result = 0x9C; break;
+	case 0x017E: result = 0x9E; break;
+	case 0x0178: result = 0x9F; break;
+	}
+
+	return result;
 }
 
 inline wchar_t cp1252ToUCS2(byte cp) {
@@ -197,6 +235,8 @@ inline wchar_t cp1252ToUCS2(byte cp) {
 errno_t convertWideTextToEscapedWideText(const wchar_t* from, wchar_t** to) {
 
 	errno_t success = 0;
+	int toIndex = 0;
+	unsigned int size = 0;
 
 	/* */
 	if (from == NULL) {
@@ -205,7 +245,7 @@ errno_t convertWideTextToEscapedWideText(const wchar_t* from, wchar_t** to) {
 	}
 
 	/* */
-	unsigned int size = wcslen(from);
+	size = wcslen(from);
 
 	/* 全部エスケープしても３倍を超えることはない。１０はバッファ*/
 	*to = (wchar_t*)calloc(size * 3 + 10, sizeof(wchar_t));
@@ -216,9 +256,20 @@ errno_t convertWideTextToEscapedWideText(const wchar_t* from, wchar_t** to) {
 	}
 
 	/* */
-	int toIndex = 0;
+	toIndex = 0;
 	for (unsigned int fromIndex = 0; fromIndex < size; fromIndex++) {
-		const wchar_t cp = from[fromIndex];
+		wchar_t cp = from[fromIndex];
+
+		/* */
+		if (UCS2ToCP1252(cp) != cp) {
+			(*to)[toIndex++] = cp;
+			continue;
+		}
+
+		/* ずらす */
+		if (cp > 0x100 && cp < 0xA00) {
+			cp = cp + 0xE000;
+		}
 
 		/* 上位バイト */
 		byte high = (cp >> 8) & 0x000000FF;
@@ -226,13 +277,13 @@ errno_t convertWideTextToEscapedWideText(const wchar_t* from, wchar_t** to) {
 		/* 下位バイト */
 		byte low = cp & 0x000000FF;
 
+		byte escapeChr = 0x10;
+
 		/* 2byteじゃない */
 		if (high == 0) {
 			(*to)[toIndex++] = cp;
 			continue;
 		}
-
-		byte escapeChr = 0x10;
 
 		/* high byteより決定 */
 		switch (high) {
@@ -281,69 +332,73 @@ A:
 	return success;
 }
 
-errno_t convertWideTextToText(const wchar_t* from, char **to ) {
-	 
-	 errno_t success = 0;
+errno_t convertWideTextToText(const wchar_t* from, char **to) {
 
-	 /* */
-	 if (from == NULL) {
-		 success = 1;
-		 goto A;
-	 }
+	errno_t success = 0;
+	int size = 0;
+	int err = 0;
 
-	 /* */
-	 int size = WideCharToMultiByte(
-		 CP_UTF8,
-		 0,
-		 from,
-		 -1,
-		 NULL,
-		 0,
-		 NULL,
-		 NULL
-	 );
+	/* */
+	if (from == NULL) {
+		success = 1;
+		goto A;
+	}
 
-	 if (size == NULL) {
-		 success = 2;
-		 goto A;
-	 }
+	/* */
+	size = WideCharToMultiByte(
+		CP_UTF8,
+		0,
+		from,
+		-1,
+		NULL,
+		0,
+		NULL,
+		NULL
+	);
 
-	 /* */
-	 *to = (char*)malloc(size * sizeof(char));
-	 if (*to == NULL) {
-		 success = 3;
-		 goto A;
-	 }
+	if (size == NULL) {
+		success = 2;
+		goto A;
+	}
 
-	 /* */
-	 int err = WideCharToMultiByte(
-		 CP_UTF8,
-		 0,
-		 from,
-		 -1,
-		 *to,
-		 size,
-		 NULL,
-		 NULL
-	 );
+	/* */
+	*to = (char*)malloc(size * sizeof(char));
+	if (*to == NULL) {
+		success = 3;
+		goto A;
+	}
 
-	 if (err == NULL) {
-		 success = 4;
-		 goto B;
-	 }
+	/* */
+	err = WideCharToMultiByte(
+		CP_UTF8,
+		0,
+		from,
+		-1,
+		*to,
+		size,
+		NULL,
+		NULL
+	);
 
-	 goto A;
+	if (err == NULL) {
+		success = 4;
+		goto B;
+	}
+
+	goto A;
 
 B:
-	 free(*to);
-	
+	free(*to);
+
 A:
-	 return success;
+	return success;
 }
 
 errno_t attachWildCard(const wchar_t* from, wchar_t ** to) {
 
 	errno_t error = 0;
+	unsigned int size = 0;
+	errno_t err = 0;
 
 	/* */
 	if (from == NULL) {
@@ -352,7 +407,7 @@ errno_t attachWildCard(const wchar_t* from, wchar_t ** to) {
 	}
 
 	/* */
-	unsigned int size = wcslen(from);
+	size = wcslen(from);
 
 	/* */
 	*to = (wchar_t*)calloc(size + 3, sizeof(wchar_t));
@@ -363,7 +418,7 @@ errno_t attachWildCard(const wchar_t* from, wchar_t ** to) {
 	}
 
 	/* */
-	errno_t err = wmemcpy_s(*to, size, from, size);
+	err = wmemcpy_s(*to, size, from, size);
 	if (err) {
 		error = 3;
 		goto B;
@@ -383,23 +438,24 @@ A:
 	return error;
 }
 
-errno_t attachBOM(const char* from, char ** to ) {
+errno_t attachBOM(const char* from, char ** to) {
 
-	errno_t success = 0;
+	errno_t error = 0;
+	unsigned int size = 0;
 
 	/* */
 	if (from == NULL) {
-		success = 1;
+		error = 1;
 		goto A;
 	}
-	
-	/* */
-	unsigned int size = strlen(from);
 
 	/* */
-	*to = (char*)calloc(size +4, sizeof(char));
+	size = strlen(from);
+
+	/* */
+	*to = (char*)calloc(size + 4, sizeof(char));
 	if (*to == NULL) {
-		success = 2;
+		error = 2;
 		goto A;
 	}
 
@@ -410,7 +466,7 @@ errno_t attachBOM(const char* from, char ** to ) {
 
 	/* */
 	if (memcpy_s(*to + 3, size + 4, from, size)) {
-		success = 3;
+		error = 3;
 		goto B;
 	}
 
@@ -421,19 +477,20 @@ B:
 	free(*to);
 
 A:
-	return success;
+	return error;
 }
 
 /* Cpp code */
-const std::wregex FILE_NAME_PATTERN(L"^(.+)\.utf8b(\.[a-zA-Z0-9]+)$");
-inline errno_t getBaseFileName(const wchar_t *source, wchar_t **to) {
+const std::wregex FILE_NAME_PATTERN1(L"^(.+)\.utf8b(\.[a-zA-Z0-9]+)$");
+const std::wregex FILE_NAME_PATTERN2(L"^(.+)\.(\.[a-zA-Z0-9_]+).utf8$");
+inline errno_t getBaseFileName(const wchar_t *source, wchar_t **to, const std::wregex pattern) {
 
 	errno_t error = 0;
 	std::wcmatch match;
 
 	/* */
 	try {
-		if (!std::regex_match(source, match, FILE_NAME_PATTERN)) {
+		if (!std::regex_match(source, match, pattern)) {
 			/* Not match */
 			*to = NULL;
 			goto A;
@@ -461,12 +518,13 @@ A:
 	return error;
 }
 
-errno_t getFullPath(wchar_t *from[],wchar_t **to) {
+errno_t getFullPath(wchar_t *from[], wchar_t **to) {
 
 	errno_t error = 0;
+	wchar_t *err = NULL;
 
 	/* */
-	if (! (*++from)) {
+	if (!(*++from)) {
 		error = 1;
 		goto F;
 	}
@@ -479,7 +537,7 @@ errno_t getFullPath(wchar_t *from[],wchar_t **to) {
 	}
 
 	/* */
-	wchar_t *err = _wfullpath(*to, *from, _MAX_PATH);
+	err = _wfullpath(*to, *from, _MAX_PATH);
 	if (*err == NULL) {
 		error = 3;
 		goto A;
@@ -501,8 +559,9 @@ errno_t digDir(const wchar_t *source)
 	errno_t success = 0;
 	wchar_t subpath[_MAX_PATH];
 	wchar_t temp[_MAX_PATH];
+	HANDLE h;
 	WIN32_FIND_DATA lp;
-	
+
 	/* */
 	errno_t err = wcscpy_s(temp, source);
 	if (err > 0) {
@@ -511,7 +570,7 @@ errno_t digDir(const wchar_t *source)
 	}
 
 	/* */
-	HANDLE h = FindFirstFile(temp, &lp);
+	h = FindFirstFile(temp, &lp);
 	if (INVALID_HANDLE_VALUE == h) {
 		success = 2;
 		goto A;
@@ -524,10 +583,10 @@ errno_t digDir(const wchar_t *source)
 	do
 	{
 		/* isDir */
-		if((lp.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) &&
-			(wcscmp(lp.cFileName, L"..") != 0) && 
+		if ((lp.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) &&
+			(wcscmp(lp.cFileName, L"..") != 0) &&
 			(wcscmp(lp.cFileName, L".") != 0)
-		){
+			) {
 			/* TODO:check len */
 			int len = wsprintf(subpath, L"%s%s\\*", temp, lp.cFileName);
 
@@ -543,26 +602,47 @@ errno_t digDir(const wchar_t *source)
 		if ((lp.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) != FILE_ATTRIBUTE_DIRECTORY) {
 			/* */
 			wchar_t *baseFileName;
-			errno_t err3 = getBaseFileName(lp.cFileName, &baseFileName);
+			errno_t err3 = getBaseFileName(lp.cFileName, &baseFileName,FILE_NAME_PATTERN1);
 			if (err3 > 0) {
 				success = 4;
 				goto B;
 			}
 
 			/* */
-			if (baseFileName == NULL) continue;
+			if (baseFileName != NULL) {
+				/* TODO:check len */
+				wchar_t fileFullPath[_MAX_PATH];
+				int len = wsprintf(fileFullPath, L"%s%s", temp, lp.cFileName);
 
-			/* TODO:check len */
-			wchar_t fileFullPath[_MAX_PATH];
-			int len = wsprintf(fileFullPath, L"%s%s", temp, lp.cFileName);
+				/* */
+				errno_t errSub = subUTF8B(fileFullPath, baseFileName);
 
-			/* */
-			errno_t errSub = sub(fileFullPath, baseFileName);
+				free(baseFileName);
 
-			free(baseFileName);
+				if (errSub > 0) {
+					goto B;
+				}
+			}
+			else {
+				errno_t err4 = getBaseFileName(lp.cFileName, &baseFileName, FILE_NAME_PATTERN2);
+				if (err4 > 0) {
+					success = 4;
+					goto B;
+				}
+				if (baseFileName != NULL) {
+					/* TODO:check len */
+					wchar_t fileFullPath[_MAX_PATH];
+					int len = wsprintf(fileFullPath, L"%s%s", temp, lp.cFileName);
 
-			if (errSub > 0) {
-				goto B;
+					/* */
+					errno_t errSub = subUTF8(fileFullPath, baseFileName);
+
+					free(baseFileName);
+
+					if (errSub > 0) {
+						goto B;
+					}
+				}
 			}
 		}
 	} while (FindNextFile(h, &lp));
@@ -591,11 +671,9 @@ int wmain(int argc, wchar_t *argv[], wchar_t *envp[])
 	/* */
 	error = attachWildCard(fullPath, &attachWildCardFullPath);
 	if (error > 0) {
-		printf("ワイルドカード添付異常:%d",error);
+		printf("ワイルドカード添付異常:%d", error);
 		goto B;
 	}
-
-	wprintf(attachWildCardFullPath);
 
 	/* */
 	error = digDir(attachWildCardFullPath);
@@ -614,9 +692,63 @@ A:
 	return 0;
 }
 
+errno_t saveTextToBinary2(const wchar_t *source, const wchar_t *baseFileName, const char* from) {
+
+	errno_t success = 0;
+	int file_size = 0;
+	errno_t fopen_err = 0;
+
+	if (source == NULL) {
+		success = 1;
+		goto F;
+	}
+
+	wchar_t drive[_MAX_DRIVE];
+	wchar_t dir[_MAX_DIR];
+	wchar_t fname[_MAX_FNAME];
+	wchar_t ext[_MAX_EXT];
+
+	/* */
+	_wsplitpath_s(
+		source,
+		drive,
+		dir,
+		fname,
+		ext
+	);
+
+	/* */
+	wchar_t exportPath[_MAX_EXT];
+	_wmakepath_s(exportPath, drive, dir, fname, L"");
+
+	/* */
+	FILE *fw;
+	fopen_err = _wfopen_s(&fw, exportPath, L"wb");
+
+	if (fopen_err) {
+		success = 3;
+		goto F;
+	}
+
+	/* */
+	file_size = fwrite(from, sizeof(char), strlen(from), fw);
+
+	if (file_size != strlen(from)) {
+		success = 4;
+		goto A;
+	}
+
+A:
+	fclose(fw);
+F:
+	return success;
+}
+
 errno_t saveTextToBinary(const wchar_t *source, const wchar_t *baseFileName, const char* from) {
 
 	errno_t success = 0;
+	int file_size = 0;
+	errno_t fopen_err = 0;
 
 	if (source == NULL) {
 		success = 1;
@@ -643,7 +775,7 @@ errno_t saveTextToBinary(const wchar_t *source, const wchar_t *baseFileName, con
 
 	/* */
 	FILE *fw;
-	errno_t fopen_err = _wfopen_s(&fw, exportPath, L"wb");
+	fopen_err = _wfopen_s(&fw, exportPath, L"wb");
 
 	if (fopen_err) {
 		success = 3;
@@ -651,7 +783,7 @@ errno_t saveTextToBinary(const wchar_t *source, const wchar_t *baseFileName, con
 	}
 
 	/* */
-	int file_size = fwrite(from, sizeof(char), strlen(from), fw);
+	file_size = fwrite(from, sizeof(char), strlen(from), fw);
 
 	if (file_size != strlen(from)) {
 		success = 4;
@@ -664,10 +796,11 @@ F:
 	return success;
 }
 
+
 /* */
-errno_t sub(const wchar_t *source, wchar_t *baseFileName){
+errno_t subUTF8(const wchar_t *source, wchar_t *baseFileName) {
 	errno_t ans = 0;
-	
+
 	char *importText = NULL;
 	char *noBOMtext = NULL;
 	wchar_t *wideNoBOMtext = NULL;
@@ -678,7 +811,82 @@ errno_t sub(const wchar_t *source, wchar_t *baseFileName){
 	/* */
 	ans = loadTextFromBinary(source, &importText);
 	if (ans > 0) {
-		printf("ファイルからテキストを読み込めない:%d",ans);
+		printf("ファイルからテキストを読み込めない:%d", ans);
+		goto A;
+	}
+
+	/* */
+	ans = convertTextToWideText(importText, &wideNoBOMtext);
+	if (ans > 0) {
+		printf("M->Wが変換できなかった:%d", ans);
+		goto C;
+	}
+
+	/* */
+	ans = convertWideTextToEscapedWideText(wideNoBOMtext, &escapedNoBOMWideText);
+	if (ans > 0) {
+		printf("エスケープに失敗した:%d", ans);
+		goto D;
+	}
+
+	/* */
+	ans = convertWideTextToText(escapedNoBOMWideText, &escapedNoBOMText);
+	if (ans > 0) {
+		printf("W->Mが変換できなかった:%d", ans);
+		goto E;
+	}
+
+	/* */
+	ans = attachBOM(escapedNoBOMText, &escapedText);
+	if (ans > 0) {
+		printf("BOMをつけるのに失敗:%d", ans);
+		goto F;
+	}
+
+	/* */
+	ans = saveTextToBinary2(source, baseFileName, escapedText);
+	if (ans > 0) {
+		printf("ファイルをセーブするのに失敗:%d", ans);
+	}
+
+G:
+	free(escapedText);
+
+F:
+	free(escapedNoBOMText);
+
+E:
+	free(escapedNoBOMWideText);
+
+D:
+	free(wideNoBOMtext);
+
+C:
+	free(noBOMtext);
+
+B:
+	free(importText);
+
+A:
+	return ans;
+}
+
+
+/* */
+errno_t subUTF8B(const wchar_t *source, wchar_t *baseFileName) {
+	errno_t ans = 0;
+
+	char *importText = NULL;
+	char *noBOMtext = NULL;
+	wchar_t *wideNoBOMtext = NULL;
+	wchar_t *escapedNoBOMWideText = NULL;
+	char *escapedNoBOMText = NULL;
+	char* escapedText = NULL;
+
+	/* */
+	ans = loadTextFromBinary(source, &importText);
+	if (ans > 0) {
+		printf("ファイルからテキストを読み込めない:%d", ans);
 		goto A;
 	}
 
@@ -692,21 +900,21 @@ errno_t sub(const wchar_t *source, wchar_t *baseFileName){
 	/* */
 	ans = convertTextToWideText(noBOMtext, &wideNoBOMtext);
 	if (ans > 0) {
-		printf("M->Wが変換できなかった:%d",ans);
+		printf("M->Wが変換できなかった:%d", ans);
 		goto C;
 	}
 
 	/* */
 	ans = convertWideTextToEscapedWideText(wideNoBOMtext, &escapedNoBOMWideText);
 	if (ans > 0) {
-		printf("エスケープに失敗した:%d",ans);
+		printf("エスケープに失敗した:%d", ans);
 		goto D;
 	}
 
 	/* */
 	ans = convertWideTextToText(escapedNoBOMWideText, &escapedNoBOMText);
 	if (ans > 0) {
-		printf("W->Mが変換できなかった:%d",ans);
+		printf("W->Mが変換できなかった:%d", ans);
 		goto E;
 	}
 
@@ -746,6 +954,3 @@ B:
 A:
 	return ans;
 }
-
-
-
